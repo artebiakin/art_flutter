2d3e3dbbb63008a59b4b89ca6df71518c5818917
Oleksandr Artebiakin <artebiakin@MacBook-Pro.local>
2020-04-28 13:58:49 +0300

    scroll changed

--- lib/screens/MainScreen.dart
+++ lib/screens/MainScreen.dart
@@ -5,58 +5,80 @@ import 'package:art_flutter/widgets/MAppBar.dart';
 import 'package:art_flutter/widgets/MBusinessCard.dart';
 import 'package:art_flutter/widgets/MListTransaction.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final double offset = MediaQuery.of(context).size.height * .15;
-
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
-      body: CustomPaint(
-        painter: BGPaint(),
-        child: CustomScrollView(
-          physics: BouncingScrollPhysics(),
-          slivers: <Widget>[
-            MAppBar(title: 'Transaction report'),
-            SliverFillRemaining(
-              hasScrollBody: false,
-              fillOverscroll: true,
-              child: Container(
-                margin: EdgeInsets.only(top: offset),
-                child: Stack(
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.only(
-                          top: offset,
-                          right: padding_horizontal,
-                          left: padding_horizontal),
-                      decoration: BoxDecoration(
-                          color: white,
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(40),
-                              topRight: Radius.circular(40))),
-                      child: MListTransaction(transactions: _initTransaction()),
+      body: Body(),
+    );
+  }
+}
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  ScrollController _scrollController;
+  double scroll = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController()..addListener(() => setOffset());
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  setOffset() {
+    setState(() {
+      scroll = _scrollController.offset;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: BGPaint(scroll),
+      child: CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        controller: _scrollController,
+        slivers: <Widget>[
+          MAppBar(title: 'Transaction report'),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            fillOverscroll: true,
+            child: Container(
+              child: Stack(
+                children: <Widget>[
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: padding_horizontal),
+                    child: Wrap(
+                      children: <Widget>[
+                        MBusinessCard(card: _initBusinessCard()),
+                        MListTransaction(transactions: _initTransaction()),
+                      ],
                     ),
-                    Container(
-                      transform: Matrix4.translationValues(0, -offset, 0),
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: padding_horizontal),
-                        child: MBusinessCard(card: _initBusinessCard()),
-                      ),
-                    ),
-                  ],
-                ),
+                  )
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 
-/* 
+  /* 
   Init transaction list.
  */
   List<Transaction> _initTransaction() {
@@ -107,13 +129,24 @@ class MainScreen extends StatelessWidget {
   Draw white block with rounded edges.
  */
 class BGPaint extends CustomPainter {
+  double scroll;
+
+  BGPaint(this.scroll);
+
   @override
   void paint(Canvas canvas, Size size) {
+    final double defaultHeight = size.height * .26;
+    final double maxHeight = size.height * .06;
+
+    double height = defaultHeight - scroll;
+
+    if (height < maxHeight) height = maxHeight;
+
     Paint paint = Paint()..color = white;
 
     canvas.drawRRect(
         RRect.fromRectAndCorners(
-            Rect.fromLTRB(0.0, size.height, size.width, size.height / 1.6),
+            Rect.fromLTRB(0.0, size.height, size.width, height),
             topLeft: Radius.circular(40),
             topRight: Radius.circular(40)),
         paint);
--- lib/widgets/MAppBar.dart
+++ lib/widgets/MAppBar.dart
@@ -3,77 +3,113 @@ import 'package:flutter/material.dart';
 
 import '../globalVariables.dart';
 
-class MAppBar extends StatelessWidget with PreferredSizeWidget {
-  // sets app bar heights
-  final double appBarHeight = 70;
+class MAppBar extends StatefulWidget {
   final String title;
-
   MAppBar({Key key, this.title}) : super(key: key);
+
+  @override
+  _MAppBarState createState() => _MAppBarState(title);
+}
+
+class _MAppBarState extends State<MAppBar> {
+  final double appBarHeight = 70;
+  final double hiddenAppBarHeight = 40;
+  final String title;
+  final GlobalKey _appBarContainer = GlobalKey();
+  bool isHiddenAppBar = false;
+  double heightBar;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPersistentFrameCallback((_) => getHeightBar());
+  }
+
+  void getHeightBar() {
+    RenderBox box = _appBarContainer.currentContext.findRenderObject();
+    double height = box.size.height;
+
+    if (heightBar == null) heightBar = height;
+
+    if (heightBar > height && !isHiddenAppBar) {
+      setState(() {
+        isHiddenAppBar = true;
+      });
+    } else if (heightBar == height && isHiddenAppBar) {
+      setState(() {
+        isHiddenAppBar = false;
+      });
+    }
+  }
+
+  _MAppBarState(this.title);
 
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(
-      backgroundColor: Colors.transparent,
+      elevation: 0,
+      backgroundColor: dark_purple,
       expandedHeight: appBarHeight,
+      bottom: PreferredSize(
+          child: SizedBox(),
+          preferredSize: Size.fromHeight(hiddenAppBarHeight)),
+      floating: true,
+      pinned: true,
       flexibleSpace: Padding(
         padding: EdgeInsets.symmetric(horizontal: padding_horizontal),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            Wrap(
-              crossAxisAlignment: WrapCrossAlignment.center,
-              spacing: 10,
-              children: <Widget>[
-                Container(
-                  width: 17,
-                  height: 17,
-                  child: CustomPaint(
-                    painter: ArrowBack(),
-                  ),
+        child: Padding(
+          key: _appBarContainer,
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: isHiddenAppBar
+              ? Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      title,
+                      style: fontStyleTitle,
+                    )
+                  ],
+                )
+              : Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Wrap(
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                      spacing: 10,
+                      children: <Widget>[
+                        Container(
+                          width: 17,
+                          height: 17,
+                          child: CustomPaint(
+                            painter: ArrowBack(),
+                          ),
+                        ),
+                        Text(
+                          title,
+                          style: fontStyleTitle,
+                        ),
+                      ],
+                    )
+                  ],
                 ),
-                Text(
-                  title,
-                  style: fontStyleTitle,
-                ),
-              ],
-            )
-          ],
         ),
       ),
     );
-    // return AppBar(
-    //   backgroundColor: Colors.transparent,
-    //   elevation: 0,
-    //   flexibleSpace: Container(
-    //     padding: EdgeInsets.symmetric(horizontal: padding_horizontal),
-    //     child: Column(
-    //       crossAxisAlignment: CrossAxisAlignment.start,
-    //       mainAxisAlignment: MainAxisAlignment.end,
-    //       children: <Widget>[
-    //         Wrap(
-    //           crossAxisAlignment: WrapCrossAlignment.center,
-    //           spacing: 10,
-    //           children: <Widget>[
-    //             Container(
-    //               width: 17,
-    //               height: 17,
-    //               child: CustomPaint(
-    //                 painter: ArrowBack(),
-    //               ),
-    //             ),
-    //             Text(
-    //               title,
-    //               style: fontStyleTitle,
-    //             ),
-    //           ],
-    //         )
-    //       ],
-    //     ),
-    //   ),
-    // );
   }
-
-  @override
-  Size get preferredSize => Size.fromHeight(appBarHeight);
 }
+
+//  extends StatelessWidget with PreferredSizeWidget {
+//   // sets app bar heights
+//   final double appBarHeight = 70;
+//   final String title;
+
+//   MAppBar({Key key, this.title}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+
+//   @override
+//   Size get preferredSize => Size.fromHeight(appBarHeight);
+// }
